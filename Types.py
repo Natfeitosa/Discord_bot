14ebcaafc4b3a283567430ae539ad1b34f41c58a
Natanael Feitosa <47866276+Natfeitosa@users.noreply.github.com>
2021-06-03 13:46:50 -0400

    Add files via upload

---
+++ Types.py
@@ -0,0 +1,5 @@
+def Types(type):
+    accepted_types = ["Bug","Dark","Dragon","Electric","Fairy","Fighting","Fire","Flying","Ghost","Grass","Ground","Ice","Ice","Normal","Poison","Psychic","Rock","Steel","Water"]
+    for a in accepted_types:
+        if(type==a):
+            return a;
